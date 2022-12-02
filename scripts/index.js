1e77010ec3d44723e36ce349055f29b680160674
Vitaly Tsyganov <format911@yandex.ru>
2022-12-02 15:15:01 +0300

    feat: добавить JavaScript в popup

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const editButton = document.querySelector('.profile__edit-button');
+
+let profileName = document.querySelector('.profile__title');
+let profileAbout = document.querySelector('.profile__subtitle');
+
+const popup = document.querySelector('.popup');
+const popupForm = popup.querySelector('.popup__container')
+const closebutton = popup.querySelector('.popup__close-button');
+const saveButton = popup.querySelector('.popup__save-button');
+
+let inputName = popup.querySelector('.popup__input_type_name');
+let inputAbout = popup.querySelector('.popup__input_type_about')
+
+function openPopup () {
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
+  popup.classList.add('popup_opened');
+}
+
+function closePopup () {
+  popup.classList.remove('popup_opened');
+}
+
+function savePopup (evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
+  closePopup();
+}
+
+editButton.addEventListener('click', openPopup);
+closebutton.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', savePopup);
