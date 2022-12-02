6fbec1bfa3dbbdcfab5ef75a1923af88e9ae2715
Vitaly Tsyganov <format911@yandex.ru>
2022-12-03 00:56:00 +0300

    fix: Исправить замечания после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,10 @@
 const editButton = document.querySelector('.profile__edit-button');
-
 let profileName = document.querySelector('.profile__title');
 let profileAbout = document.querySelector('.profile__subtitle');
 
 const popup = document.querySelector('.popup');
 const popupForm = popup.querySelector('.popup__container')
-const closebutton = popup.querySelector('.popup__close-button');
-const saveButton = popup.querySelector('.popup__save-button');
-
+const closeButton = popup.querySelector('.popup__close-button');
 let inputName = popup.querySelector('.popup__input_type_name');
 let inputAbout = popup.querySelector('.popup__input_type_about')
 
@@ -29,5 +26,5 @@ function savePopup (evt) {
 }
 
 editButton.addEventListener('click', openPopup);
-closebutton.addEventListener('click', closePopup);
+closeButton.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', savePopup);
