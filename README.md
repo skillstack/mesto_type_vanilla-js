f0c464ab6b4e6a22e06037bbb5326a918c0b12d3
Vitaly Tsyganov <format911@yandex.ru>
2023-01-14 03:19:20 +0300

    feat: добавить функционал 6ПР

--- README.md
+++ README.md
@@ -11,11 +11,14 @@
 5. Flexbox;
 6. Grid layout;
 7. Работа с DOM;
-8. Адаптивная верстка;
-9. Семантические тэги.
+8. Валидация форм на JS;
+9. Адаптивная верстка;
+10. Семантические тэги.
 ## Планы по доработке проекта:
-- добавить новый функционал после шестого спринта;
+- Провести рефакторинг кода;
+- Добавить в README.md расширенное описание.
 ## Ссылки:
-* [Ссылка на макет 4ПР в Figma](https://www.figma.com/file/AJlNiRkANk4KLBYfCQ3H5x/JavaScript.-Sprint-4?t=TRIKOiG8frPu6IPE-1)
-*  [Ссылка на макет 5ПР в Figma](https://www.figma.com/file/EZJqgRoqXP6lV4O5xzHj3U/JavaScript.-Sprint-5?node-id=50160%3A460&t=8nU1GbhytpSWFVY4-1)
+* [Ссылка на макет 4 спринт в Figma](https://www.figma.com/file/AJlNiRkANk4KLBYfCQ3H5x/JavaScript.-Sprint-4?t=TRIKOiG8frPu6IPE-1)
+* [Ссылка на макет 5 спринт в Figma](https://www.figma.com/file/EZJqgRoqXP6lV4O5xzHj3U/JavaScript.-Sprint-5?node-id=50160%3A460&t=8nU1GbhytpSWFVY4-1)
+* [Ссылка на макет 6 спринт в Figma](https://www.figma.com/file/3rmeBseVO5pCEbT6jxCKS7/JavaScript.-Sprint-6?node-id=0%3A1&t=H7dbvq2ttG9QAsOj-1)
 * [Ссылка на проект](https://skillstack.github.io/mesto/)
--- scripts/index.js
+++ scripts/index.js
@@ -1,79 +1,85 @@
-//Кнопки закрытия popup
+// Конфиг для валидации
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-button',
+  inactiveButtonClass: 'popup__save-button_inactive',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active'
+};
+
+// Кнопки закрытия popup
 const popupCloseButtons = document.querySelectorAll('.popup__close-button');
 
-//Поля профиля на странице
+// Список всех popup
+const popupList = document.querySelectorAll('.popup');
+
+// Поля профиля на странице
 const profileName = document.querySelector('.profile__title');
 const profileAbout = document.querySelector('.profile__subtitle');
 
-//Кнопка редактирования профиля
+// Кнопка редактирования профиля
 const profileEditButton = document.querySelector('.profile__edit-button');
 
-//Popup редактирования профиля
+// Popup редактирования профиля
 const popupProfile = document.querySelector('.popup_type_profile');
 const popupFormProfile = popupProfile.querySelector('.popup__form');
 const inputName = popupProfile.querySelector('.popup__input_type_name');
 const inputAbout = popupProfile.querySelector('.popup__input_type_about');
 
-//Галерея для вставки карточки места
+// Галерея для вставки карточки места
 const gallery = document.querySelector('.gallery');
 
-//Шаблон карточки места
+// Шаблон карточки места
 const cardTemplate = document.querySelector('#cardTemplate').content;
 
-//Кнопка добавления карточки места
+// Кнопка добавления карточки места
 const cardAddButton = document.querySelector('.profile__add-button');
 
-//Popup создания карточки места
+// Popup создания карточки места
 const popupCard = document.querySelector('.popup_type_card');
 const popupFormCard = popupCard.querySelector('.popup__form');
 const inputPlace = popupCard.querySelector('.popup__input_type_place');
 const inputLink = popupCard.querySelector('.popup__input_type_link');
 
-//Popup фотографии места
+// Popup фотографии места
 const popupFigure = document.querySelector('.popup_type_img');
 const popupImg = popupFigure.querySelector('.popup__img');
-const popupCaption = popupFigure.querySelector('.popup__caption')
-
-//Первичный массив карточек мест
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+const popupCaption = popupFigure.querySelector('.popup__caption');
 
 // Открытие и закрытие popup
+function closePopupEsc(evt) {
+  if (evt.key === 'Escape') {
+    popupList.forEach((popup) => {
+      if (popup.classList.contains('popup_opened')) {
+        closePopup(popup);
+      };
+    });
+  };
+};
+
 function openPopup(popup) {
+  enableValidation(validationConfig);
   popup.classList.add('popup_opened');
+  document.addEventListener('keydown', closePopupEsc);
 };
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupEsc);
 };
 
 popupCloseButtons.forEach((popupCloseButton) => {
   popupCloseButton.addEventListener('click', (evt) => {
     closePopup(evt.currentTarget.closest('.popup'));
+  });
+});
+
+popupList.forEach((popup) => {
+  popup.addEventListener('mousedown', (evt) => {
+    if (evt.target.classList.contains('popup_opened')) {
+      closePopup(popup);
+    };
   });
 });
 
@@ -126,7 +132,7 @@ function createCard(name, link) {
   });
 
   return cardElement;
-}
+};
 
 // Получение данных из формы создания карточки места
 cardAddButton.addEventListener('click', () => {
@@ -146,3 +152,5 @@ popupFormCard.addEventListener('submit', savePopupCard);
 initialCards.forEach((item) => {
   gallery.append(createCard(item.name, item.link));
 });
+
+enableValidation(validationConfig);
